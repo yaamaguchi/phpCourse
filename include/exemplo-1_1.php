b9475cfa1e014ac3d466dd2f587729c4b63cc0cc
Matheus Yamaguchi <Matheus Yamaguchi>
2017-03-29 21:01:43 -0300

    aula 29-03 .2

--- include/exemplo-1_1.php
+++ include/exemplo-1_1.php
@@ -1,6 +1,13 @@
 <?php 
+	//   -> 
+	
+	//include "inc/exemplo-1.php";
+	//require "inc/exemplo-1.php";
 
-	include "inc/exemplo-1.php";
+	//include_once "inc/exemplo-1.php";
+
+	require_once "inc/exemplo-1.php";
+
 
 	$resultado = somar(10,20);
 	echo $resultado;
